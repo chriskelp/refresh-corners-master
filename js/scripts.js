3475ba18ad348d4566643e1e4eda312e586349c0
Chris Kelp <chris.m.kelp@gmail.com>
2014-10-20 13:31:17 -0700

    corner_Refresh

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+  // Your Scripts
+
+  $('#one').corner("bevel");
+  $('#two').corner('30px');
+  $('#three').corner("wicked");
+});
+
